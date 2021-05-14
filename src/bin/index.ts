1100956dda41e25080e7dd1263d00d900c1dac59
matzkoh <matzkoh@gmail.com>
2021-05-14 18:09:39 +0900

    chore: bump deps

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -5,7 +5,7 @@ import pkgDir from 'pkg-dir'
 pkgDir(__dirname)
   .then(dir => {
     if (dir) {
-      spawn((electronPath as unknown) as string, [dir])
+      spawn(electronPath as unknown as string, [dir])
     }
   })
   .catch(console.error)
