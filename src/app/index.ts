dc73c4ab906d1371e26f4df67615e9e9e48e8610
matzkoh <matzkoh@gmail.com>
2021-11-29 04:43:59 +0900

    fix: text

--- src/app/index.ts
+++ src/app/index.ts
@@ -15,4 +15,4 @@ app.on('window-all-closed', () => {})
 
 const port = typeof process.env.PORT === 'string' ? parseInt(process.env.PORT, 10) : undefined
 
-initServer(port).then(() => console.log(`listening on port ${port}`), console.error)
+initServer(port).then(wss => console.log(`listening on port ${wss.options.port}`), console.error)
